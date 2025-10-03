06f0807f96b2767fbe84113a77a11a4be88fbcde
AndreToral <jrodriguez16@upao.edu.pe>
2025-10-03 12:51:15 -0500

    chore(alb): deshabilitar listener HTTPS y redirección temporalmente

--- infra/alb.tf
+++ infra/alb.tf
@@ -45,28 +45,15 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
-  default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = var.acm_certificate_arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.frontend.arn
   }
 }
+
+# HTTPS listener and redirect disabled while testing without ACM certificate.
+# resource "aws_lb_listener" "https" { ... }
 
 resource "aws_lb_listener_rule" "api_routing" {
   listener_arn = aws_lb_listener.https.arn
